724605749faf0ed67f240a8c0dfd52417142b408
bigVL <84589629+BIGVl@users.noreply.github.com>
2021-11-06 15:41:29 +0200

    Fixed the bug with the equal sign, there is one more if you press an operator after an operator and still need to add styling

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const numbers = document.querySelectorAll('#number');
 const display = document.querySelector('.display');
 const operators = document.querySelectorAll('#operator');
 const result = document.querySelector('.result');
-const clear = document.querySelector('.clear');
+const clear = document.querySelector('.allClear');
+const deleteOne = document.querySelector('.clear');
+const dot = document.querySelector('.dot');
+
+
+//Deletes one number from the display's text content
+function deleting () {
+  display.textContent = display.textContent.substring(0,display.textContent.length-1);
+  previousNumber='';
+};
+deleteOne.addEventListener('click',deleting);
 
 //Clears the calculator
 function clearing () {
@@ -14,6 +24,15 @@ function clearing () {
 }
 clear.addEventListener('click', clearing);
 
+//Adds a dot the number displayed
+function addingDot () {
+  if (display.textContent==='') return display.textContent='0';
+  if (display.textContent.includes('.')) return;
+
+  display.textContent+= '.';
+}
+dot.addEventListener('click',addingDot);
+
 //Adds to the display each time a number is pressed 
 let previousNumber='';
 let currentNumber='';
@@ -21,12 +40,12 @@ let operator='';
 
 numbers.forEach(element =>{
   element.addEventListener('click',()=>{
-    if (display.textContent.length>9) return null;
+    if (display.textContent.length>9) return display.textContent="TOO MUCH!!";
     display.textContent+= element.textContent; 
     if (currentNumber!==''){
       display.textContent='';
       currentNumber='';
-      display.textContent= element.textContent;
+      display.textContent+= element.textContent;
     } 
   })
 });
@@ -41,9 +60,10 @@ operators.forEach(element=>{
    previousNumber= display.textContent;
    display.textContent='';
  }
- else if (currentNumber==='') {
+ else if (currentNumber==='' && operator!=='') {
    currentNumber= display.textContent;
    display.textContent= operate(operator,previousNumber,currentNumber);
+   if (display.textContent.length>9) return display.textContent="TOO MUCH!!";
    previousNumber = display.textContent;
    operator=element.textContent;
 
@@ -56,8 +76,10 @@ result.addEventListener('click', ()=>{
     currentNumber= display.textContent;
   }
   display.textContent= operate(operator,previousNumber,currentNumber);
-  
+  if (display.textContent.length>9) return display.textContent="TOO MUCH!!";
+  previousNumber='';
   currentNumber= '';
+  operator='';
  
 })
 
