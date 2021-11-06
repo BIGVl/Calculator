06959854ecc4c6665a848a42fd2aca308259b3a0
bigVL <84589629+BIGVl@users.noreply.github.com>
2021-11-06 14:24:57 +0200

    The calculator is working, need to fix a little bug with the equal sign, add a few more buttons and make it look nice

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
-//Settings the DOM
-const numbers = Array.from(document.querySelectorAll('#number'));
+//Setting the DOM
+const numbers = document.querySelectorAll('#number');
 const display = document.querySelector('.display');
-const operators = Array.from(document.querySelectorAll('#operator'));
+const operators = document.querySelectorAll('#operator');
 const result = document.querySelector('.result');
+const clear = document.querySelector('.clear');
 
-
+//Clears the calculator
+function clearing () {
+  display.textContent='';
+  previousNumber='';
+  currentNumber=''; 
+  operator='';
+}
+clear.addEventListener('click', clearing);
 
 //Adds to the display each time a number is pressed 
-let firstNumber;
-let secondNumber;
-let operand;
+let previousNumber='';
+let currentNumber='';
+let operator='';
 
 numbers.forEach(element =>{
   element.addEventListener('click',()=>{
-    display.textContent+= element.textContent;
+    if (display.textContent.length>9) return null;
+    display.textContent+= element.textContent; 
+    if (currentNumber!==''){
+      display.textContent='';
+      currentNumber='';
+      display.textContent= element.textContent;
+    } 
   })
 });
-//Stores the display value and resets it, also stores what button was pressed
+//Stores the display value and resets it, also stores what .operator button was pressed
 operators.forEach(element=>{
   element.addEventListener('click',()=>{
-    firstNumber = display.textContent;
-    firstNumber=parseInt(firstNumber);
-    display.textContent='';  
-    if(element.className='add') {
-      operand = '+';
-    }
-    else if (element.className='substract') {
-      operand = '-';
-    }
-    else if (element.className='multiply') {
-      operand = '*';
-    }
-    else if (element.className='divide') {
-      operand = '/';
-    };
-  })
+
+ if (operator==='') {
+   operator = element.textContent;
+ }
+ if (previousNumber==='') {
+   previousNumber= display.textContent;
+   display.textContent='';
+ }
+ else if (currentNumber==='') {
+   currentNumber= display.textContent;
+   display.textContent= operate(operator,previousNumber,currentNumber);
+   previousNumber = display.textContent;
+   operator=element.textContent;
+
+ };
+})
 })
 //Makes the operation happen 
 result.addEventListener('click', ()=>{
-  secondNumber= display.textContent;
-  secondNumber=parseInt(secondNumber);
-  operate(operand,firstNumber,secondNumber);
-  console.log(operate(operand,firstNumber,secondNumber));
+  if(currentNumber===''){
+    currentNumber= display.textContent;
+  }
+  display.textContent= operate(operator,previousNumber,currentNumber);
+  
+  currentNumber= '';
+ 
 })
 
 
@@ -71,7 +88,7 @@ function operate (operator, a, b) {
           return add(a, b)
         case '-':
           return substract(a, b)
-        case '*':
+        case 'x':
           return multiply(a, b)
         case '/':
           if (b === 0) return null
